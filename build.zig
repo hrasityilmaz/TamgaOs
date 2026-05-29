503246ead767efdf51fa176f1333f55776f1c788
hrasity <hrasity@gmail.com>
2026-05-30 00:11:31 +0300

    Add C build issue

--- build.zig
+++ build.zig
@@ -9,6 +9,7 @@ pub fn build(b: *std.Build) void {
 
     const optimize = b.standardOptimizeOption(.{});
 
+    // Zig kernel
     const kernel = b.addExecutable(.{
         .name = "kernel",
         .root_module = b.createModule(.{
@@ -25,4 +26,29 @@ pub fn build(b: *std.Build) void {
     kernel.root_module.single_threaded = true;
 
     b.installArtifact(kernel);
+
+    const c_kernel = b.addExecutable(.{
+        .name = "c_kernel",
+        .root_module = b.createModule(.{
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+
+    c_kernel.root_module.addCSourceFile(.{
+        .file = b.path("src/C/kernel.c"),
+        .flags = &.{
+            "-ffreestanding",
+            "-fno-stack-protector",
+            "-fno-builtin",
+        },
+    });
+
+    c_kernel.root_module.addAssemblyFile(b.path("src/multiboot.s"));
+    c_kernel.setLinkerScript(b.path("linker.ld"));
+    c_kernel.entry = .{ .symbol_name = "_start" };
+    c_kernel.root_module.red_zone = false;
+    c_kernel.root_module.single_threaded = true;
+
+    b.installArtifact(c_kernel);
 }
