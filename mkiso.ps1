32b2a50d72364cfb286bc5bc9e827483b8c38828
hrasity <hrasity@gmail.com>
2026-06-01 01:01:41 +0300

    zig kernel mkiso

--- mkiso.ps1
+++ mkiso.ps1
@@ -1,8 +1,10 @@
+Remove-Item -Force .\TamgaOS.iso -ErrorAction SilentlyContinue
+
 xorriso.exe -as mkisofs `
   -b boot/limine/limine-bios-cd.bin `
   -no-emul-boot `
   -boot-load-size 4 `
   -boot-info-table `
   --protective-msdos-label `
-  .\iso `
-  -o .\TamgaOS.iso
+  -o .\TamgaOS.iso `
+  .\iso
