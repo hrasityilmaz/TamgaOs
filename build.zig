f7b06933c03ad75b113e22c812d9446fe3156103
hrasity <hrasity@gmail.com>
2026-06-01 01:01:10 +0300

    Added sse disable and C file update

--- build.zig
+++ build.zig
@@ -5,33 +5,49 @@ pub fn build(b: *std.Build) void {
         .cpu_arch = .x86,
         .os_tag = .freestanding,
         .abi = .none,
+
+        // For gdt
+        .cpu_features_sub = std.Target.x86.featureSet(&.{
+            .mmx,
+            .sse,
+            .sse2,
+            .sse3,
+            .ssse3,
+            .sse4_1,
+            .sse4_2,
+            .avx,
+            .avx2,
+        }),
     });
 
     const optimize = b.standardOptimizeOption(.{});
 
-    // Zig kernel
+    // Zig
     const kernel = b.addExecutable(.{
         .name = "kernel",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
+            .red_zone = false,
+            .single_threaded = true,
         }),
     });
 
     kernel.root_module.addAssemblyFile(b.path("src/multiboot.s"));
     kernel.setLinkerScript(b.path("linker.ld"));
     kernel.entry = .{ .symbol_name = "_start" };
-    kernel.root_module.red_zone = false;
-    kernel.root_module.single_threaded = true;
 
     b.installArtifact(kernel);
 
+    // C
     const c_kernel = b.addExecutable(.{
         .name = "c_kernel",
         .root_module = b.createModule(.{
             .target = target,
             .optimize = optimize,
+            .red_zone = false,
+            .single_threaded = true,
         }),
     });
 
@@ -44,11 +60,18 @@ pub fn build(b: *std.Build) void {
         },
     });
 
+    c_kernel.root_module.addCSourceFile(.{
+        .file = b.path("src/C/gdt.c"),
+        .flags = &.{
+            "-ffreestanding",
+            "-fno-stack-protector",
+            "-fno-builtin",
+        },
+    });
+
     c_kernel.root_module.addAssemblyFile(b.path("src/multiboot.s"));
     c_kernel.setLinkerScript(b.path("linker.ld"));
     c_kernel.entry = .{ .symbol_name = "_start" };
-    c_kernel.root_module.red_zone = false;
-    c_kernel.root_module.single_threaded = true;
 
     b.installArtifact(c_kernel);
 }
