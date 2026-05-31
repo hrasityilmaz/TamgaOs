8b36181666de0a5122f4612d0edd65d05071afc6
hrasity <hrasity@gmail.com>
2026-06-01 01:03:03 +0300

    added print and gdt

--- src/C/kernel.c
+++ src/C/kernel.c
@@ -1,14 +1,24 @@
+#include "gdt.h"
 
-void _start(void)
-{
-  volatile unsigned short *VGA = (volatile unsigned short *) 0xB8000;
-  const char *msg = "KERNEL OK __C__";
+static volatile unsigned short *const VGA = (volatile unsigned short *)0xB8000;
 
-  for(int i = 0; msg[i] != '\0'; i++){
-    VGA[i] = (0x0F << 8) | msg[i];
+static void print(const char *msg, unsigned int offset) {
+  unsigned int i = 0;
+
+  while (msg[i] != '\0') {
+    VGA[offset + i] = (0x0F << 8) | msg[i];
+    i++;
   }
+}
 
-  for(;;){
+void _start(void) {
+  gdt_init();
+  volatile unsigned short *VGA = (volatile unsigned short *)0xB8000;
+
+  print("TamgaOS __C__", 0);
+  print("GDT OK __C__", 80);
+  print("Kernel OK __C__", 160);
+  for (;;) {
     __asm__ volatile("hlt");
   }
 }
