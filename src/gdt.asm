0c76f57df6a47781bc03bb87c0d047c3608d5b56
hrasity <hrasity@gmail.com>
2026-06-08 12:58:31 +0300

    gdt error all file asm :)

--- src/gdt.asm
+++ src/gdt.asm
@@ -17,7 +17,7 @@ gdt_kernel_code:
         dw 0x0000      ;base[15:0]
         db 0x00        ;base[23:16]
         db 10011010b   ;P=1 DPL=00 Type=1010 (code, readable, non-conforming)
-        db 11011111b   ;G = 1 S = 1 Type = 1010
+        db 11001111b   ;G=1 D/B=1 (32-bit) AVL=0 L=0 | limit[19:16]=1111
         db 0x00        ;base[32:24]
 
 gdt_kernel_data:
