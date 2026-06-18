332496ed4d47314aff263495a0c6f105f56e157e
hrasity <hrasity@gmail.com>
2026-06-18 12:13:19 +0300

    sys clock general file dont use extern

---
+++ kernels/arm/include/system_clock.h
@@ -0,0 +1,21 @@
+#ifndef SYSTEM_CLOCK_H
+#define SYSTEM_CLOCK_H
+
+/*
+ * system_clock.h
+ * TamgaOS - System Clock
+ * PLL: 50MHz / 15 * 36 = 120MHz
+ */
+
+#ifndef CORE_CLOCK_HZ
+#define CORE_CLOCK_HZ    (120000000UL)  /* PLL (OUTDIV1 = /1) */
+#endif
+
+#define EXTAL0_HZ        (50000000UL)   /* Harici kristal      */
+#define BUS_CLOCK_HZ     (60000000UL)   /* Core / 2            */
+#define FLEXBUS_CLOCK_HZ (40000000UL)   /* Core / 3            */
+#define FLASH_CLOCK_HZ   (24000000UL)   /* Core / 5            */
+#define UART0_CLOCK_HZ   CORE_CLOCK_HZ  /* UART0 core clk      */
+#define PIT_CLOCK_HZ     BUS_CLOCK_HZ   /* PIT bus clk         */
+
+#endif /* SYSTEM_CLOCK_H */
