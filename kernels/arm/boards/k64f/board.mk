7c730183b2429f57ed30ee76094d6d0ce5f6478d
hrasity <hrasity@gmail.com>
2026-06-18 12:08:10 +0300

    Updages to fix clock issue

--- kernels/arm/boards/k64f/board.mk
+++ kernels/arm/boards/k64f/board.mk
@@ -1,10 +1,12 @@
 # boards/k64f/board.mk
-
 CPU_FLAGS  = -mcpu=cortex-m4
+CPU_FLAGS += -mthumb
 CPU_FLAGS += -mfpu=fpv4-sp-d16
 CPU_FLAGS += -mfloat-abi=hard
-CPU_FLAGS += -mthumb
 
-STARTUP    = boards/k64f/startup_k64f.s
-LDSCRIPT   = boards/k64f/linker.ld
+STARTUP      = boards/k64f/startup_k64f.s
+LDSCRIPT     = boards/k64f/linker.ld
 BOARD_DEFINE = BOARD_K64F
+
+CPU_FLAGS += -DCORE_CLOCK_HZ=120000000UL
+#CPU_FLAGS += -DUSE_PLL
