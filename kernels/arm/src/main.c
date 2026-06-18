5a6bf87db11324afeab219414dcb568058d6aae2
hrasity <hrasity@gmail.com>
2026-06-18 12:10:49 +0300

    main file updates to new drivers

--- kernels/arm/src/main.c
+++ kernels/arm/src/main.c
@@ -1,45 +1,65 @@
-#include "MK64F12REGS.h"
+#include "mcg.h"
 #include "mmio_deviation.h"
+#include "pit.h"
+#include "system_clock.h"
 #include "uart.h"
 
-#define LED_RED_PIN_SHIFT (22U)
-#define LED_RED_PIN_MASK (1UL << LED_RED_PIN_SHIFT) // PTB22
-#define LED_DELAY_COUNT (500000UL)
+#define LED_RED_PIN (1UL << 22U)
 
-volatile uint32_t g_systick_count = 0U;
+static void disable_wdog(void) {
+  WDOG->UNLOCK = WDOG_UNLOCK_KEY1;
+  WDOG->UNLOCK = WDOG_UNLOCK_KEY2;
+  WDOG->STCTRLH = WDOG_STCTRLH_DISABLE;
+}
 
-void SysTick_Handler(void) { g_systick_count++; }
+static void board_init(void) {
+  /* LED - PTB22 */
+  SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;
+  PORTB->PCR[22] = PORT_PCR_MUX(1U);
+  GPIOB->PDDR |= LED_RED_PIN;
+}
 
-// static void delay(volatile uint32_t n) {
-//   while (n != 0U) {
-//     n--;
-//   }
-// }
-
-static void systick_init(void) {
-  SYSTICK->LOAD = ((CORE_CLOCK_HZ / 1000UL) - 1UL) & SYSTICK_LOAD_RELOAD_MASK;
-  SYSTICK->VAL = 0U;
-  SYSTICK->CTRL = SYSTICK_CTRL_CLKSOURCE_MASK | SYSTICK_CTRL_TICKINT_MASK |
-                  SYSTICK_CTRL_ENABLE_MASK;
+/* TODO: LATER REMOBE */
+static void uart_print_u32(uint32_t val) {
+  char buf[11];
+  uint8_t i = 0U;
+  if (val == 0U) {
+    uart0_putc('0');
+    return;
+  }
+  while (val > 0U && i < 10U) {
+    buf[i++] = (char)('0' + (char)(val % 10U));
+    val /= 10U;
+  }
+  while (i > 0U) {
+    uart0_putc(buf[--i]);
+  }
 }
 
 int main(void) {
-  SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;               // Clock
-  PORTB->PCR[LED_RED_PIN_SHIFT] = PORT_PCR_MUX(1U); // GPIO
-  GPIOB->PDDR |= LED_RED_PIN_MASK;                  // Output
+  // TODO: startup....
+  disable_wdog(); // problem on startup fix!!!
+  mcg_init_120mhz();
+
   uart0_init(115200U);
-  uart0_puts("TamgaOS booted!\n");
-  systick_init();
+  uart0_puts("TamgaOS booting...\r\n");
 
+  board_init();
+
+  pit_init(1000U);
+  uart0_puts("PIT started\r\n");
+
+  uint32_t next_tick = pit_get_tick();
   while (1) {
-    // GPIOB->PTOR = LED_RED_PIN_MASK;
-    // delay(LED_DELAY_COUNT);
-    //  uart0_puts("TamgaOS tick!\n");
-
-    if (g_systick_count >= 1000U) {
-      uart0_puts("TAMGAOS Tick systick = % 1000 ( 1sec )\n");
-      g_systick_count = 0U;
-      GPIOB->PTOR = LED_RED_PIN_MASK;
+    next_tick += 500U;
+    while (pit_get_tick() < next_tick) {
     }
+
+    GPIOB->PTOR = LED_RED_PIN;
+    uart0_puts("tick=");
+    uart_print_u32(pit_get_tick());
+    uart0_puts("\r\n");
   }
+
+  return 0; // dead maybe remove
 }
