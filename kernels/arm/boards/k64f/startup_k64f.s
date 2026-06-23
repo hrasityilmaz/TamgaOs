bf4022a6e5c0ab822e41fd56dbc75536b906adfc
hrasity <hrasity@gmail.com>
2026-06-23 15:19:24 +0300

    added data copy area

--- kernels/arm/boards/k64f/startup_k64f.s
+++ kernels/arm/boards/k64f/startup_k64f.s
@@ -8,7 +8,7 @@ _vectors:
     .word  _stack_top
     .word  Reset_Handler
     .word  Default_Handler
-    .word  Default_Handler
+    .word  HardFault_Handler
     .word  Default_Handler
     .word  Default_Handler
     .word  Default_Handler
@@ -117,28 +117,38 @@ _vectors:
 .thumb_func
 .global Reset_Handler
 Reset_Handler:
-    /* 1. Watchdog'u kapat - BSS dongusunden once */
-    /* WDOG_UNLOCK: 0x40052000 + 0x0E = 0x4005200E */
+    /* 1. WDOG unlock + disable */
     ldr  r0, =0x4005200EU
     ldr  r1, =0xC520
     strh r1, [r0]
     ldr  r1, =0xD928
     strh r1, [r0]
-    /* WDOG_STCTRLH: 0x40052000, bit0=0 disable */
     ldr  r0, =0x40052000U
     ldr  r1, =0x0010
     strh r1, [r0]
 
-    /* 2. BSS sifirla */
+    /* .data: Flash -> RAM */
+    ldr  r0, =_data_start
+    ldr  r1, =_data_end
+    ldr  r2, =_data_load
+    cmp  r0, r1
+    beq  .Lbss_init
+.Lcopy_loop:
+    ldr  r3, [r2], #4
+    str  r3, [r0], #4
+    cmp  r0, r1
+    blt  .Lcopy_loop
+
+    /* 3. BSS zeroed */
+.Lbss_init:
     ldr  r0, =_bss_start
     ldr  r1, =_bss_end
-    subs r2, r1, r0
-    beq  .Lcall_main
-    mov  r1, #0
+    mov  r2, #0
 .Lbss_loop:
-    str  r1, [r0], #4
-    subs r2, r2, #4
-    bgt  .Lbss_loop
+    cmp  r0, r1
+    bge  .Lcall_main
+    str  r2, [r0], #4
+    b    .Lbss_loop
 
 .Lcall_main:
     bl   main
@@ -172,6 +182,12 @@ SysTick_Handler:
 .thumb_func
 .global PendSV_Handler
 PendSV_Handler:
+    b    Default_Handler
+
+.weak HardFault_Handler
+.thumb_func
+.global HardFault_Handler
+HardFault_Handler:
     b    Default_Handler
 
 .end
