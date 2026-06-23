4cb0e2071824cd115c47f690de8625468f98187d
hrasity <hrasity@gmail.com>
2026-06-23 15:22:45 +0300

    added new functions

--- kernels/arm/include/scheduler.h
+++ kernels/arm/include/scheduler.h
@@ -2,15 +2,14 @@
 #define SCHEDULER_H
 
 #include "task.h"
+#include "sched_critical.h"
 #include <stdint.h>
 
-// Defines on assembly
+// Defined in assembly (pendsv_handler.s)
 extern task_t *volatile g_current_task;
 extern task_t *volatile g_next_task;
 
 void sched_init(void);
-uint32_t sched_critical_enter(void);
-void sched_critical_exit(uint32_t old_basepri);
 int8_t sched_task_create(void (*func)(void), uint8_t priority);
 void sched_delay_ms(uint32_t ms);
 void sched_task_pause(uint8_t index);
