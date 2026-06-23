77f509bf6e1693f3428c6e5ee2bc143964b114b4
hrasity <hrasity@gmail.com>
2026-06-23 15:20:54 +0300

    changed to nonblocking

--- kernels/arm/drivers/k64f/uart.c
+++ kernels/arm/drivers/k64f/uart.c
@@ -3,6 +3,11 @@
 #include "uart.h"
 #include <stdint.h>
 
+#define UART_TX_BUF_SIZE (256U)
+static volatile uint8_t s_tx_buf[UART_TX_BUF_SIZE];
+static volatile uint32_t s_tx_head;
+static volatile uint32_t s_tx_tail;
+
 void uart0_init(uint32_t baud) {
   SIM->SCGC4 |= SIM_SCGC4_UART0_MASK;
   SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;
@@ -14,26 +19,46 @@ void uart0_init(uint32_t baud) {
   PORTB->PCR[17] = PORT_PCR_MUX(3U);
 
   UART0->C2 = 0U;
-
   UART0->BDH = 0U;
-  UART0->BDL = (uint8_t)(UART0_CLOCK_HZ / (16L * baud));
+  UART0->BDL = (uint8_t)(UART0_CLOCK_HZ / (16UL * baud));
   UART0->C4 = 0U;
-
   UART0->C1 = 0U;
+
+  s_tx_head = 0U;
+  s_tx_tail = 0U;
+
   UART0->C2 = UART_C2_TE_MASK | UART_C2_RE_MASK;
+
+  // TODO: change to header!!
+  volatile uint32_t *nvic_iser = (volatile uint32_t *)0xE000E100UL;
+  nvic_iser[0] |= (1UL << 31U);
+}
+
+// from startup.s
+void UART0_RX_TX_IRQHandler(void) {
+  if (UART0->S1 & UART_S1_TDRE_MASK) {
+    if (s_tx_tail != s_tx_head) {
+      UART0->D = s_tx_buf[s_tx_tail];
+      s_tx_tail = (s_tx_tail + 1U) % UART_TX_BUF_SIZE;
+    } else {
+      UART0->C2 &= ~(1U << 7U);
+    }
+  }
 }
 
 void uart0_putc(char c) {
-  while ((UART0->S1 & UART_S1_TDRE_MASK) == 0U) {
+  uint32_t next_head = (s_tx_head + 1U) % UART_TX_BUF_SIZE;
+  while (next_head == s_tx_tail) {
   }
-  UART0->D = (uint8_t)c;
+  s_tx_buf[s_tx_head] = (uint8_t)c;
+  s_tx_head = next_head;
+  UART0->C2 |= (1U << 7U);
 }
 
 void uart0_puts(const char *s) {
   while (*s != '\0') {
-    if (*s == '\n') {
+    if (*s == '\n')
       uart0_putc('\r');
-    }
     uart0_putc(*s);
     s++;
   }
