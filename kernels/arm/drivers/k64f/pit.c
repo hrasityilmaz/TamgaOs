caffb5795d1e32ed5da2dc3b9622db4dd94f58a9
hrasity <hrasity@gmail.com>
2026-06-23 15:20:36 +0300

    Updated pit.c

--- kernels/arm/drivers/k64f/pit.c
+++ kernels/arm/drivers/k64f/pit.c
@@ -1,34 +1,35 @@
 #include "MK64F12REGS.h"
 #include "mmio_deviation.h"
 #include "pit.h"
+#include "scheduler.h"
 #include "system_clock.h"
+#include <stddef.h>
 #include <stdint.h>
 
-static volatile uint32_t g_pit_tick = 0U;
+static volatile uint32_t g_pit_tick;
 
 void pit_init(uint32_t period_us) {
   SIM->SCGC6 |= SIM_SCGC6_PIT_MASK;
   PIT->MCR = PIT_MCR_FRZ_MASK;
-
   PIT->CHANNEL[0].LDVAL = ((PIT_CLOCK_HZ / 1000000UL) * period_us) - 1UL;
   PIT->CHANNEL[0].TFLG = PIT_TFLG_TIF_MASK;
   PIT->CHANNEL[0].TCTRL = PIT_TCTRL_TEN_MASK | PIT_TCTRL_TIE_MASK;
 
-  volatile uint32_t *nvic_iser = (volatile uint32_t *)0xE000E100UL;
+  volatile uint32_t *nvic_iser =
+      (volatile uint32_t *)0xE000E100UL; // TODO: header change!!
   nvic_iser[PIT0_IRQ_NUMBER / 32U] = (1UL << (PIT0_IRQ_NUMBER % 32U));
 }
 
 uint32_t pit_get_tick(void) { return g_pit_tick; }
 
 void pit_delay_ms(uint32_t ms) {
-    uint32_t start = pit_get_tick();
-    uint32_t now;
-    do {
-        now = pit_get_tick();
-    } while ((now - start) < ms);
+  uint32_t start = pit_get_tick();
+  while ((pit_get_tick() - start) < ms) {
+  }
 }
 
 void PIT0_IRQHandler(void) {
-    PIT->CHANNEL[0].TFLG = PIT_TFLG_TIF_MASK;
-    g_pit_tick++;
+  PIT->CHANNEL[0].TFLG = PIT_TFLG_TIF_MASK;
+  g_pit_tick++;
+  sched_tick();
 }
