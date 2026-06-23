9eaa70a7221f01f09741b4105ed40f79c41b442e
hrasity <hrasity@gmail.com>
2026-06-23 14:12:02 +0300

    new files updated for makefile

--- kernels/arm/boards/k64f/board.mk
+++ kernels/arm/boards/k64f/board.mk
@@ -1,15 +1,15 @@
-# boards/k64f/board.mk
 CPU_FLAGS  = -mcpu=cortex-m4
 CPU_FLAGS += -mthumb
-CPU_FLAGS += -mfpu=fpv4-sp-d16
-CPU_FLAGS += -mfloat-abi=hard
+CPU_FLAGS += -mfloat-abi=soft
 
 STARTUP      = boards/k64f/startup_k64f.s
 LDSCRIPT     = boards/k64f/linker.ld
 BOARD_DEFINE = BOARD_K64F
 
 CPU_FLAGS += -DCORE_CLOCK_HZ=120000000UL
-#CPU_FLAGS += -DUSE_PLL
 
-C_SRCS += kernel/scheduler.c
+AS_SRCS += kernel/arch/cortex_m4/sched_start.s
 AS_SRCS += kernel/arch/cortex_m4/pendsv_handler.s
+
+C_SRCS  += kernel/core/scheduler.c
+C_SRCS  += drivers/k64f/pit.c drivers/k64f/uart.c drivers/k64f/mcg.c
