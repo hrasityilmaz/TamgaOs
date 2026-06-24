a861f4b2064c3bbed42726b8762fb9ae058ef0db
hrasity <hrasity@gmail.com>
2026-06-24 13:10:29 +0300

    priority set

--- kernels/arm/drivers/k64f/uart.c
+++ kernels/arm/drivers/k64f/uart.c
@@ -4,37 +4,32 @@
 #include <stdint.h>
 
 #define UART_TX_BUF_SIZE (256U)
-static volatile uint8_t s_tx_buf[UART_TX_BUF_SIZE];
+static volatile uint8_t  s_tx_buf[UART_TX_BUF_SIZE];
 static volatile uint32_t s_tx_head;
 static volatile uint32_t s_tx_tail;
 
 void uart0_init(uint32_t baud) {
   SIM->SCGC4 |= SIM_SCGC4_UART0_MASK;
   SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;
-
   SIM->SOPT2 = (SIM->SOPT2 & ~SIM_SOPT2_UART0SRC_MASK) |
                SIM_SOPT2_UART0SRC(SIM_SOPT2_UART0SRC_MCGFLL);
-
   PORTB->PCR[16] = PORT_PCR_MUX(3U);
   PORTB->PCR[17] = PORT_PCR_MUX(3U);
-
   UART0->C2 = 0U;
   UART0->BDH = 0U;
   UART0->BDL = (uint8_t)(UART0_CLOCK_HZ / (16UL * baud));
-  UART0->C4 = 0U;
-  UART0->C1 = 0U;
-
+  UART0->C4  = 0U;
+  UART0->C1  = 0U;
   s_tx_head = 0U;
   s_tx_tail = 0U;
 
   UART0->C2 = UART_C2_TE_MASK | UART_C2_RE_MASK;
-
-  // TODO: change to header!!
+  volatile uint8_t *nvic_ipr = (volatile uint8_t *)0xE000E400UL;
+  nvic_ipr[31] = 0x80U;
   volatile uint32_t *nvic_iser = (volatile uint32_t *)0xE000E100UL;
   nvic_iser[0] |= (1UL << 31U);
 }
 
-// from startup.s
 void UART0_RX_TX_IRQHandler(void) {
   if (UART0->S1 & UART_S1_TDRE_MASK) {
     if (s_tx_tail != s_tx_head) {
@@ -48,8 +43,7 @@ void UART0_RX_TX_IRQHandler(void) {
 
 void uart0_putc(char c) {
   uint32_t next_head = (s_tx_head + 1U) % UART_TX_BUF_SIZE;
-  while (next_head == s_tx_tail) {
-  }
+  while (next_head == s_tx_tail) {}
   s_tx_buf[s_tx_head] = (uint8_t)c;
   s_tx_head = next_head;
   UART0->C2 |= (1U << 7U);
@@ -57,8 +51,7 @@ void uart0_putc(char c) {
 
 void uart0_puts(const char *s) {
   while (*s != '\0') {
-    if (*s == '\n')
-      uart0_putc('\r');
+    if (*s == '\n') uart0_putc('\r');
     uart0_putc(*s);
     s++;
   }
