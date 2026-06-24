68fc1c1c3c70375fa1193c8e7dee1facab111ac8
hrasity <hrasity@gmail.com>
2026-06-24 13:11:46 +0300

    added mutex

--- kernels/arm/include/task.h
+++ kernels/arm/include/task.h
@@ -3,29 +3,32 @@
 
 #include <stdint.h>
 
-#define TASK_MAX        (4U)   /* 8'den 4'e indirdik */
-#define TASK_STACK_SIZE (128U) /* 256'dan 128'e indirdik - 512 byte */
+//
+// 2KB x 8
+//
+#define TASK_MAX (8U)
+#define TASK_STACK_SIZE (512U)
 
-#define TASK_PRIORITY_HIGH   (0U)
+#define TASK_PRIORITY_HIGH (0U)
 #define TASK_PRIORITY_NORMAL (1U)
-#define TASK_PRIORITY_LOW    (2U)
-#define TASK_PRIORITY_IDLE   (3U)
+#define TASK_PRIORITY_LOW (2U)
+#define TASK_PRIORITY_IDLE (3U)
 
 typedef enum {
-    TASK_READY   = 0U,
-    TASK_RUNNING = 1U,
-    TASK_BLOCKED = 2U,
-    TASK_PAUSED  = 3U,
-    TASK_DEAD    = 4U
+  TASK_READY = 0U,
+  TASK_RUNNING = 1U,
+  TASK_BLOCKED = 2U,
+  TASK_PAUSED = 3U,
+  TASK_DEAD = 4U
 } task_state_t;
 
 typedef struct {
-    uint32_t    *sp;
-    task_state_t state;
-    uint8_t      priority;
-    uint32_t     delay_ticks;
-    void        (*func)(void);
-    uint32_t     stack[TASK_STACK_SIZE];
+  uint32_t *sp;
+  task_state_t state;
+  uint8_t priority;
+  uint32_t delay_ticks;
+  void (*func)(void);
+  uint32_t stack[TASK_STACK_SIZE];
 } task_t;
 
 #endif
