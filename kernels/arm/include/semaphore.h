b57d4ca77664328573b2fa89391a611b21ea7502
hrasity <hrasity@gmail.com>
2026-06-24 13:13:43 +0300

    added semaphore header

---
+++ kernels/arm/include/semaphore.h
@@ -0,0 +1,16 @@
+#ifndef SEMAPHORE_H
+#define SEMAPHORE_H
+
+#include <stdint.h>
+
+typedef struct {
+  volatile int32_t count;
+  int32_t max;
+} sem_t;
+
+void sem_init(sem_t *s, int32_t initial, int32_t max);
+void sem_take(sem_t *s);
+void sem_give(sem_t *s);
+int sem_trytake(sem_t *s);
+
+#endif
