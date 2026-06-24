a4bd4cb6272894e7eb003ce5a1162f106a92f90b
hrasity <hrasity@gmail.com>
2026-06-24 13:10:59 +0300

    added callback function for semaphore

--- kernels/arm/include/pit.h
+++ kernels/arm/include/pit.h
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 
+typedef void (*pit_callback_t)(void);
+void pit_set_callback(pit_callback_t cb);
 void pit_init(uint32_t period_us);
 uint32_t pit_get_tick(void);
 void pit_delay_ms(uint32_t ms);
