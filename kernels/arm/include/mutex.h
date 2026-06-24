354e472e4cb8ab4ce1fed06c372f226de5c6d3d4
hrasity <hrasity@gmail.com>
2026-06-24 13:13:21 +0300

    mutex header file

---
+++ kernels/arm/include/mutex.h
@@ -0,0 +1,17 @@
+#ifndef MUTEX_H
+#define MUTEX_H
+
+#include "task.h"
+#include <stddef.h>
+#include <stdint.h>
+
+typedef struct {
+  volatile task_t *task;
+} mutex_t;
+
+void mutex_init(mutex_t *m);
+void mutex_lock(mutex_t *m);
+void mutex_unlock(mutex_t *m);
+int mutex_trylock(mutex_t *m);
+
+#endif
