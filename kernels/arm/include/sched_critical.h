3dda5152bb12d3262044ac2e19b468973768c5c8
hrasity <hrasity@gmail.com>
2026-06-25 14:42:14 +0300

    removed basepri so complicated later will implement

--- kernels/arm/include/sched_critical.h
+++ kernels/arm/include/sched_critical.h
@@ -7,21 +7,15 @@
 extern "C" {
 #endif
 
-#define __NVIC_PRIO_BITS 4U
-#define SCHED_MAX_SYSCALL_PRIO (3U << (8U - __NVIC_PRIO_BITS))
-
 static inline uint32_t sched_critical_enter(void) {
-  uint32_t old_basepri;
-  __asm volatile("mrs %0, basepri" : "=r"(old_basepri));
-  __asm volatile("msr basepri, %0" ::"r"(SCHED_MAX_SYSCALL_PRIO) : "memory");
-  __asm volatile("dsb");
-  __asm volatile("isb");
-  return old_basepri;
+    uint32_t primask;
+    __asm volatile("mrs %0, primask" : "=r"(primask));
+    __asm volatile("cpsid i");
+    return primask;
 }
 
-static inline void sched_critical_exit(uint32_t old_basepri) {
-  __asm volatile("msr basepri, %0" ::"r"(old_basepri) : "memory");
-  __asm volatile("isb");
+static inline void sched_critical_exit(uint32_t primask) {
+    __asm volatile("msr primask, %0" :: "r"(primask));
 }
 
 #ifdef __cplusplus
