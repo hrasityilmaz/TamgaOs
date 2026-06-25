fd155d2ea456c3313cb24f12469e0c7736e00cac
hrasity <hrasity@gmail.com>
2026-06-25 14:41:33 +0300

    Added new function define here

--- kernels/arm/include/pit.h
+++ kernels/arm/include/pit.h
@@ -6,6 +6,7 @@
 typedef void (*pit_callback_t)(void);
 void pit_set_callback(pit_callback_t cb);
 void pit_init(uint32_t period_us);
+void pit_sched_enable(void);
 uint32_t pit_get_tick(void);
 void pit_delay_ms(uint32_t ms);
 
