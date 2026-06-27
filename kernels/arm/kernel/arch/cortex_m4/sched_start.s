930d4609e0a8df33e009704494d80c8b5afa06b7
hrasity <hrasity@gmail.com>
2026-06-27 15:14:29 +0300

    test to load

--- kernels/arm/kernel/arch/cortex_m4/sched_start.s
+++ kernels/arm/kernel/arch/cortex_m4/sched_start.s
@@ -15,13 +15,15 @@ sched_start_asm:
     isb
     mov     r0, #2
     msr     control, r0
-    isb
+    isb  # if pending interrupt
     cpsie   i
     isb
     
-    /* r1: R0(0),R1(4),R2(8),R3(12),R12(16),LR(20),PC(24),xPSR(28) */
+    /* r1: R0(0),R1(4),R2(8),R3(12),R12(16),LR(20),PC(24),xPSR(28) */:
+    /* mrs r1, psp  */       
+    /* ldr r2, [r1, #24] */    
     ldr     r2, [r1, #24]
-    bx      r2 /* bx lr !!!! */
+    bx      r2 /* not bx lr !!!! */
 
 .size sched_start_asm, . - sched_start_asm
 .end
