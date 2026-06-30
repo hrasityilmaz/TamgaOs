de6bb91f0b77d04873cb6f324b1686fc06412805
hrasity <hrasity@gmail.com>
2026-06-30 19:52:27 +0300

    added water list to mutex.h

--- kernels/arm/include/mutex.h
+++ kernels/arm/include/mutex.h
@@ -6,7 +6,9 @@
 #include <stdint.h>
 
 typedef struct {
-  volatile task_t *task;
+  // volatile task_t *task;
+  task_t *volatile task;
+  task_t *waiters;
 } mutex_t;
 
 void mutex_init(mutex_t *m);
