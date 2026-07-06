87137cc426e79e12296a1165d0419c8e4073c5fa
hrasity <hrasity@gmail.com>
2026-07-06 08:12:01 +0300

    function definations updated

--- kernels/arm/include/k64f/uart.h
+++ kernels/arm/include/k64f/uart.h
@@ -4,8 +4,8 @@
 #include <stdint.h>
 #include "system_clock.h"
 
-void uart0_init(uint32_t baud);
-void uart0_putc(char c);
-void uart0_puts(const char *s);
+void uart_init(uint32_t baud);
+void uart_putc(char c);
+void uart_puts(const char *s);
 
 #endif
