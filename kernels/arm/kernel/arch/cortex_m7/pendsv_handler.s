234553610aef624fe5361003c2539f54e7112975
hrasity <hrasity@gmail.com>
2026-07-07 08:33:10 +0300

    Add EXC_RETURN (LR) save/restore to PendSV handler, Lazy FPU context: tst lr, #0x10 check for S16-S31

--- kernels/arm/kernel/arch/cortex_m7/pendsv_handler.s
+++ kernels/arm/kernel/arch/cortex_m7/pendsv_handler.s
@@ -1,5 +1,6 @@
 .syntax unified
 .cpu    cortex-m7
+.fpu    fpv5-d16
 .thumb
 
 .extern g_current_task
@@ -13,24 +14,36 @@
 PendSV_Handler:
     mrs     r0, psp
     isb
+
     ldr     r3, =g_current_task
     ldr     r2, [r3]
     cbz     r2, PendSV_restore
-    stmdb   r0!, {r4-r11}
-    str     r0, [r2, #0]
+
+    /* Is task using FPU? EXC_RETURN bit4=0 means yes */
+    tst     lr, #0x10
+    it      eq
+    vstmdbeq r0!, {s16-s31}         /* push high FPU regs if used */
+
+    stmdb   r0!, {r4-r11, lr}       /* push R4-R11 + EXC_RETURN */
+    str     r0, [r2, #0]            /* save SP */
 
 PendSV_restore:
     ldr     r3, =g_next_task
-    ldr     r2, [r3]    
+    ldr     r2, [r3]
     ldr     r3, =g_current_task
     str     r2, [r3]
-    ldr     r0, [r2, #0]
-    ldmia   r0!, {r4-r11}
+
+    ldr     r0, [r2, #0]            /* load next task SP */
+    ldmia   r0!, {r4-r11, lr}       /* pop R4-R11 + EXC_RETURN */
+
+    /* Is next task using FPU? */
+    tst     lr, #0x10
+    it      eq
+    vldmiaeq r0!, {s16-s31}         /* pop high FPU regs if used */
+
     msr     psp, r0
     isb
-    movw    lr, #0xFFFD
-    movt    lr, #0xFFFF
-    bx      lr
+    bx      lr                      /* EXC_RETURN controls FPU/PSP/Thread */
 
 .size PendSV_Handler, . - PendSV_Handler
 .end
