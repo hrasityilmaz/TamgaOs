79ea3fac3e048580d5ec4a5ebcfd307229878c38
hrasity <hrasity@gmail.com>
2026-07-07 08:33:56 +0300

    sched_start_asm: bx r2 (not bx lr) for first task

--- kernels/arm/kernel/arch/cortex_m7/sched_start.s
+++ kernels/arm/kernel/arch/cortex_m7/sched_start.s
@@ -1,5 +1,6 @@
 .syntax unified
 .cpu    cortex-m7
+.fpu    fpv5-d16
 .thumb
 
 .section .kernel_asm, "ax", %progbits
@@ -9,21 +10,21 @@
 
 sched_start_asm:
     cpsid   i
-    ldr     r1, [r0, #0] 
-    ldmia   r1!, {r4-r11}
+    ldr     r1, [r0, #0]            /* r1 = task->sp */
+    ldmia   r1!, {r4-r11, lr}       /* pop R4-R11 + EXC_RETURN */
+    tst     lr, #0x10
+    it      eq
+    vldmiaeq r1!, {s16-s31}
+
     msr     psp, r1
     isb
     mov     r0, #2
     msr     control, r0
-    isb  /* if pending interrupt */
+    isb
     cpsie   i
     isb
-    
-    /* r1: R0(0),R1(4),R2(8),R3(12),R12(16),LR(20),PC(24),xPSR(28) */
-    /* mrs r1, psp  */       
-    /* ldr r2, [r1, #24] */    
-    ldr     r2, [r1, #24]
-    bx      r2 /* not bx lr !!!! */
+    ldr     r2, [r1, #24]           /* PC from exception frame */
+    bx      r2                      /* jump to task — not bx lr! */
 
 .size sched_start_asm, . - sched_start_asm
 .end
