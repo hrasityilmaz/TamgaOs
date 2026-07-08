bd77980f330c2ba5f769edf7f631ac833584d2ae
hrasity <hrasity@gmail.com>
2026-07-08 08:49:10 +0300

    MPU task and Led task test

--- kernels/arm/src/Stm32h753zi/main.c
+++ kernels/arm/src/Stm32h753zi/main.c
@@ -3,7 +3,11 @@
 #include "uart.h"
 #include "scheduler.h"
 #include "task.h"
-#include <stdint.h>
+//#include <stdint.h>
+#include "i2c.h"
+#include "mpu6050.h"
+
+
 
 #define RCC_AHB4ENR (*(volatile uint32_t *)0x580244E0U)
 #define GPIOB_MODER (*(volatile uint32_t *)0x58020400U)
@@ -14,6 +18,12 @@
 #define LD1_PIN (1UL << 0U)
 #define LD2_PIN (1UL << 1U)
 #define LD3_PIN (1UL << 14U)
+
+
+// I2C
+#define MPU6050_ADDR    0x69U
+#define MPU6050_WHO_AM_I 0x75U
+
 
 static void board_init(void)
 {
@@ -26,7 +36,23 @@ static void board_init(void)
     GPIOE_ODR &= ~LD2_PIN;
 }
 
+
+void task_imu(void)
+{
+    mpu6050_data_t data;
+    while (1) {
+        sched_delay_ms(10U);
+        if (mpu6050_read(&data) == 0) {
+            uart_printf("AX:%d  AY:%d  AZ:%d  |  GX:%d  GY:%d  GZ:%d\r\n",
+                        data.accel_x, data.accel_y, data.accel_z,
+                        data.gyro_x,  data.gyro_y,  data.gyro_z);
+        }
+    }
+}
+
+
 /* Task A — float accumulates up, should always increase */
+/*
 void task_float_a(void)
 {
     float val = 0.0f;
@@ -40,8 +66,9 @@ void task_float_a(void)
         uart_printf("[A] %d.%d\r\n", i, f);
     }
 }
-
+*/
 /* Task B — float decreases down, should always decrease */
+/*
 void task_float_b(void)
 {
     float val = 100.0f;
@@ -55,13 +82,40 @@ void task_float_b(void)
         uart_printf("[B] %d.%d\r\n", i, f);
     }
 }
+*/
 
 /* Task LED — no float */
+/*
 void task_led_yellow(void)
 {
     while (1) {
         GPIOE_ODR ^= LD2_PIN;
         sched_delay_ms(250U);
+    }
+}
+*/
+
+void task_led_green(void)
+{
+    while (1) {
+        GPIOB_ODR ^= LD1_PIN;
+        sched_delay_ms(500U);
+    }
+}
+ 
+void task_led_yellow(void)
+{
+    while (1) {
+        GPIOE_ODR ^= LD2_PIN;
+        sched_delay_ms(300U);
+    }
+}
+ 
+void task_led_red(void)
+{
+    while (1) {
+        GPIOB_ODR ^= LD3_PIN;
+        sched_delay_ms(700U);
     }
 }
 
@@ -71,15 +125,59 @@ int main(void)
     systick_init(480000000U);
     board_init();
     uart_init();
+    i2c_init();
 
     uart_puts("TamgaOS STM32H753ZI @ 480MHz\r\n");
-    uart_puts("FPU context isolation test\r\n");
+    //uart_puts("FPU context isolation test\r\n");
+    //uart_puts("I2C MPU6050 Test");
+    uart_puts("MPU6050 raw data and 3 Led task together\r\n");
+
+    if (mpu6050_init() < 0) {
+        uart_puts("MPU6050 init failed!\r\n");
+        while (1) {}
+    }
+
+    uart_puts("MPU6050 OK\r\n");
+
+    /*
+    while (1) {
+        if (mpu6050_read(&data) == 0) {
+            uart_printf("AX:%d  AY:%d  AZ:%d  |  GX:%d  GY:%d  GZ:%d\r\n",
+                        data.accel_x, data.accel_y, data.accel_z,
+                        data.gyro_x,  data.gyro_y,  data.gyro_z);
+        }
+        systick_delay_ms(100U);
+    }
+    */
+
+
+    /*
+    uint8_t who = 0U;
+    int8_t ret = i2c_read(MPU6050_ADDR, MPU6050_WHO_AM_I, &who, 1U);
+    
+    if (ret < 0) {
+        uart_puts("I2C error — check wiring\r\n");
+    } else {
+        uart_printf("WHO_AM_I = 0x%x\r\n", who);
+        if (who == 0x68U) {
+            uart_puts("MPU6050 found!\r\n");
+        } else {
+            uart_puts("Unexpected device ID\r\n");
+        }
+    }
+    */
+
 
     sched_init();
 
-    sched_task_create(task_float_a,    TASK_PRIORITY_NORMAL);
-    sched_task_create(task_float_b,    TASK_PRIORITY_NORMAL);
+    //sched_task_create(task_float_a,    TASK_PRIORITY_NORMAL);
+    //sched_task_create(task_float_b,    TASK_PRIORITY_NORMAL);
+    //sched_task_create(task_led_yellow, TASK_PRIORITY_NORMAL);
+
+    sched_task_create(task_imu,        TASK_PRIORITY_HIGH); // flight data :)
+    sched_task_create(task_led_green,  TASK_PRIORITY_NORMAL);
     sched_task_create(task_led_yellow, TASK_PRIORITY_NORMAL);
+    sched_task_create(task_led_red,    TASK_PRIORITY_NORMAL);
 
     sched_start();
 
