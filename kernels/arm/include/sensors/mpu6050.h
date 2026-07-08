5ce1258e767cf60b6e1b8a7356fedcf4fd4a3c63
hrasity <hrasity@gmail.com>
2026-07-08 08:49:49 +0300

    Used sensors drivers

---
+++ kernels/arm/include/sensors/mpu6050.h
@@ -0,0 +1,28 @@
+#ifndef MPU6050_H
+#define MPU6050_H
+
+#include <stdint.h>
+
+/* MPU6050 I2C address */
+#define MPU6050_ADDR    0x69U   /* AD0=VCC if not 0x68 !! */
+
+typedef struct {
+    int16_t accel_x;
+    int16_t accel_y;
+    int16_t accel_z;
+    int16_t temp_raw;
+    int16_t gyro_x;
+    int16_t gyro_y;
+    int16_t gyro_z;
+} mpu6050_data_t;
+
+int8_t mpu6050_init(void);
+int8_t mpu6050_read(mpu6050_data_t *data);
+
+/* TODO: need to convert!!
+ * why FPU needed :)   
+ * accel: raw / 16384.0f = g  (±2g range)
+ * gyro:  raw / 131.0f   = °/s (±250°/s range)
+ * temp:  raw / 340.0f + 36.53f = °C
+ */
+#endif /* MPU6050_H */
