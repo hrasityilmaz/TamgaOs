52d505c25505c134abb0c74718380d382b7a0c17
hrasity <hrasity@gmail.com>
2026-07-10 09:43:28 +0300

    changed to generic

--- kernels/arm/kernel/core/hardfault.c
+++ kernels/arm/kernel/core/hardfault.c
@@ -1,122 +1,188 @@
 /*
- * hardfault.c
- * TamgaOS - HardFault UART debug handler
- *
- * Fault anındaki tüm bilgileri UART'a basar:
- *   PC, LR, PSR, R0-R3, R12, SP
- *   CFSR (neden fault olduğu)
- *   BFAR, MMFAR (adres hataları)
+ * kernel/core/hardfault.c
+ * TamgaOS - Generic HardFault handler (board-agnostic)
+ * uart from drivers folder if not error will throw !!!
  */
 
 #include <stdint.h>
 
-#define UART0_S1 (*(volatile uint8_t *)0x4006A004U)
-#define UART0_D (*(volatile uint8_t *)0x4006A007U)
-#define UART0_TDRE (1U << 7U)
-#define SCB_CFSR (*(volatile uint32_t *)0xE000ED28U) /* Combined fault status  \
-                                                      */
-#define SCB_HFSR (*(volatile uint32_t *)0xE000ED2CU) /* HardFault status */
-#define SCB_BFAR (*(volatile uint32_t *)0xE000ED38U) /* BusFault address */
-#define SCB_MMFAR                                                              \
-  (*(volatile uint32_t *)0xE000ED34U) /* MemManage address     */
+#define SCB_CFSR   (*(volatile uint32_t *)0xE000ED28U)
+#define SCB_HFSR   (*(volatile uint32_t *)0xE000ED2CU)
+#define SCB_DFSR   (*(volatile uint32_t *)0xE000ED30U)
+#define SCB_MMFAR  (*(volatile uint32_t *)0xE000ED34U)
+#define SCB_BFAR   (*(volatile uint32_t *)0xE000ED38U)
+#define SCB_AFSR   (*(volatile uint32_t *)0xE000ED3CU)
 
-static void hf_putc(char c) {
-  while (!(UART0_S1 & UART0_TDRE)) {
-  }
-  UART0_D = (uint8_t)c;
-}
+// GDB print hf_cfsr, print hf_pc etc
+volatile uint32_t hf_r0;
+volatile uint32_t hf_r1;
+volatile uint32_t hf_r2;
+volatile uint32_t hf_r3;
+volatile uint32_t hf_r12;
+volatile uint32_t hf_lr;
+volatile uint32_t hf_pc;
+volatile uint32_t hf_psr;
+
+volatile uint32_t hf_cfsr;
+volatile uint32_t hf_hfsr;
+volatile uint32_t hf_dfsr;
+volatile uint32_t hf_mmfar;
+volatile uint32_t hf_bfar;
+volatile uint32_t hf_afsr;
+
+// uart.c from drivers 
+extern void uart_putc(char c);
 
 static void hf_puts(const char *s) {
   while (*s)
-    hf_putc(*s++);
+    uart_putc(*s++);
 }
 
 static void hf_print_hex(uint32_t val) {
   hf_puts("0x");
   for (int i = 28; i >= 0; i -= 4) {
-    uint8_t nibble = (val >> i) & 0xFU;
-    hf_putc(nibble < 10U ? ('0' + nibble) : ('A' + nibble - 10U));
+    uint8_t nibble = (uint8_t)((val >> i) & 0xFU);
+    uart_putc(nibble < 10U ? (char)('0' + nibble) : (char)('A' + nibble - 10U));
   }
 }
 
-/* [0]=R0 [1]=R1 [2]=R2 [3]=R3 [4]=R12 [5]=LR [6]=PC [7]=xPSR */
-void hardfault_c_handler(uint32_t *frame) {
-  hf_puts("\r\n\r\n*** HARDFAULT ***\r\n");
-
-  hf_puts("PC   = ");
-  hf_print_hex(frame[6]);
+static void hf_print_reg(const char *name, uint32_t val) {
+  hf_puts(name);
+  hf_print_hex(val);
   hf_puts("\r\n");
-  hf_puts("LR   = ");
-  hf_print_hex(frame[5]);
-  hf_puts("\r\n");
-  hf_puts("xPSR = ");
-  hf_print_hex(frame[7]);
-  hf_puts("\r\n");
-  hf_puts("R0   = ");
-  hf_print_hex(frame[0]);
-  hf_puts("\r\n");
-  hf_puts("R1   = ");
-  hf_print_hex(frame[1]);
-  hf_puts("\r\n");
-  hf_puts("R2   = ");
-  hf_print_hex(frame[2]);
-  hf_puts("\r\n");
-  hf_puts("R3   = ");
-  hf_print_hex(frame[3]);
-  hf_puts("\r\n");
-  hf_puts("R12  = ");
-  hf_print_hex(frame[4]);
-  hf_puts("\r\n");
-  hf_puts("SP   = ");
-  hf_print_hex((uint32_t)frame);
-  hf_puts("\r\n");
-
-  hf_puts("CFSR = ");
-  hf_print_hex(SCB_CFSR);
-  hf_puts("\r\n");
-  hf_puts("HFSR = ");
-  hf_print_hex(SCB_HFSR);
-  hf_puts("\r\n");
-  hf_puts("BFAR = ");
-  hf_print_hex(SCB_BFAR);
-  hf_puts("\r\n");
-  hf_puts("MMFAR= ");
-  hf_print_hex(SCB_MMFAR);
-  hf_puts("\r\n");
-  uint32_t cfsr = SCB_CFSR;
-  hf_puts("--- CFSR decode ---\r\n");
-  if (cfsr & (1U << 25U))
-    hf_puts("  DIVBYZERO\r\n");
-  if (cfsr & (1U << 24U))
-    hf_puts("  UNALIGNED\r\n");
-  if (cfsr & (1U << 19U))
-    hf_puts("  NOCP (no coprocessor)\r\n");
-  if (cfsr & (1U << 18U))
-    hf_puts("  INVPC\r\n");
-  if (cfsr & (1U << 17U))
-    hf_puts("  INVSTATE\r\n");
-  if (cfsr & (1U << 16U))
-    hf_puts("  UNDEFINSTR\r\n");
-  if (cfsr & (1U << 15U))
-    hf_puts("  BFARVALID\r\n");
-  if (cfsr & (1U << 12U))
-    hf_puts("  STKERR\r\n");
-  if (cfsr & (1U << 11U))
-    hf_puts("  UNSTKERR\r\n");
-  if (cfsr & (1U << 10U))
-    hf_puts("  IMPRECISERR\r\n");
-  if (cfsr & (1U << 9U))
-    hf_puts("  PRECISERR\r\n");
-  if (cfsr & (1U << 8U))
-    hf_puts("  IBUSERR\r\n");
-  if (cfsr & (1U << 3U))
-    hf_puts("  MUNSTKERR\r\n");
-  if (cfsr & (1U << 1U))
-    hf_puts("  DACCVIOL\r\n");
-  if (cfsr & (1U << 0U))
-    hf_puts("  IACCVIOL\r\n");
-
-  hf_puts("*** HALTED ***\r\n");
-  while (1) {
-  }
 }
+
+__attribute__((naked))
+void HardFault_Handler(void)
+{
+    __asm volatile(
+        "tst lr,#4          \n"
+        "ite eq             \n"
+        "mrseq r0,msp       \n"
+        "mrsne r0,psp       \n"
+        "b hardfault_c      \n");
+}
+
+void hardfault_c(uint32_t *sp)
+{
+    hf_r0  = sp[0];
+    hf_r1  = sp[1];
+    hf_r2  = sp[2];
+    hf_r3  = sp[3];
+    hf_r12 = sp[4];
+    hf_lr  = sp[5];
+    hf_pc  = sp[6];
+    hf_psr = sp[7];
+
+    hf_cfsr  = SCB_CFSR;
+    hf_hfsr  = SCB_HFSR;
+    hf_dfsr  = SCB_DFSR;
+    hf_mmfar = SCB_MMFAR;
+    hf_bfar  = SCB_BFAR;
+    hf_afsr  = SCB_AFSR;
+
+    hf_puts("\r\n\r\n*** HARDFAULT ***\r\n");
+    hf_print_reg("PC   = ", hf_pc);
+    hf_print_reg("LR   = ", hf_lr);
+    hf_print_reg("xPSR = ", hf_psr);
+    hf_print_reg("R0   = ", hf_r0);
+    hf_print_reg("R1   = ", hf_r1);
+    hf_print_reg("R2   = ", hf_r2);
+    hf_print_reg("R3   = ", hf_r3);
+    hf_print_reg("R12  = ", hf_r12);
+    hf_print_reg("SP   = ", (uint32_t)sp);
+    hf_print_reg("CFSR = ", hf_cfsr);
+    hf_print_reg("HFSR = ", hf_hfsr);
+    hf_print_reg("DFSR = ", hf_dfsr);
+    hf_print_reg("BFAR = ", hf_bfar);
+    hf_print_reg("MMFAR= ", hf_mmfar);
+    hf_print_reg("AFSR = ", hf_afsr);
+
+    hf_puts("--- CFSR decode ---\r\n");
+    if (hf_cfsr & (1UL << 25U)) hf_puts("  DIVBYZERO\r\n");
+    if (hf_cfsr & (1UL << 24U)) hf_puts("  UNALIGNED\r\n");
+    if (hf_cfsr & (1UL << 19U)) hf_puts("  NOCP (no coprocessor)\r\n");
+    if (hf_cfsr & (1UL << 18U)) hf_puts("  INVPC\r\n");
+    if (hf_cfsr & (1UL << 17U)) hf_puts("  INVSTATE\r\n");
+    if (hf_cfsr & (1UL << 16U)) hf_puts("  UNDEFINSTR\r\n");
+    if (hf_cfsr & (1UL << 15U)) hf_puts("  BFARVALID\r\n");
+    if (hf_cfsr & (1UL << 12U)) hf_puts("  STKERR\r\n");
+    if (hf_cfsr & (1UL << 11U)) hf_puts("  UNSTKERR\r\n");
+    if (hf_cfsr & (1UL << 10U)) hf_puts("  IMPRECISERR\r\n");
+    if (hf_cfsr & (1UL <<  9U)) hf_puts("  PRECISERR\r\n");
+    if (hf_cfsr & (1UL <<  8U)) hf_puts("  IBUSERR\r\n");
+    if (hf_cfsr & (1UL <<  7U)) hf_puts("  MMARVALID\r\n");
+    if (hf_cfsr & (1UL <<  4U)) hf_puts("  MSTKERR\r\n");
+    if (hf_cfsr & (1UL <<  3U)) hf_puts("  MUNSTKERR\r\n");
+    if (hf_cfsr & (1UL <<  1U)) hf_puts("  DACCVIOL\r\n");
+    if (hf_cfsr & (1UL <<  0U)) hf_puts("  IACCVIOL\r\n");
+
+    hf_puts("*** HALTED ***\r\n");
+
+    __asm volatile("bkpt #0");
+
+    while (1) {
+    }
+}
+
+/*
+#include <stdint.h>
+
+#define SCB_CFSR   (*(volatile uint32_t *)0xE000ED28U)
+#define SCB_HFSR   (*(volatile uint32_t *)0xE000ED2CU)
+#define SCB_DFSR   (*(volatile uint32_t *)0xE000ED30U)
+#define SCB_MMFAR  (*(volatile uint32_t *)0xE000ED34U)
+#define SCB_BFAR   (*(volatile uint32_t *)0xE000ED38U)
+#define SCB_AFSR   (*(volatile uint32_t *)0xE000ED3CU)
+
+volatile uint32_t hf_r0;
+volatile uint32_t hf_r1;
+volatile uint32_t hf_r2;
+volatile uint32_t hf_r3;
+volatile uint32_t hf_r12;
+volatile uint32_t hf_lr;
+volatile uint32_t hf_pc;
+volatile uint32_t hf_psr;
+
+volatile uint32_t hf_cfsr;
+volatile uint32_t hf_hfsr;
+volatile uint32_t hf_dfsr;
+volatile uint32_t hf_mmfar;
+volatile uint32_t hf_bfar;
+volatile uint32_t hf_afsr;
+
+__attribute__((naked))
+void HardFault_Handler(void)
+{
+    __asm volatile(
+        "tst lr,#4          \n"
+        "ite eq             \n"
+        "mrseq r0,msp       \n"
+        "mrsne r0,psp       \n"
+        "b hardfault_c      \n");
+}
+
+void hardfault_c(uint32_t *sp)
+{
+    hf_r0  = sp[0];
+    hf_r1  = sp[1];
+    hf_r2  = sp[2];
+    hf_r3  = sp[3];
+    hf_r12 = sp[4];
+    hf_lr  = sp[5];
+    hf_pc  = sp[6];
+    hf_psr = sp[7];
+
+    hf_cfsr  = SCB_CFSR;
+    hf_hfsr  = SCB_HFSR;
+    hf_dfsr  = SCB_DFSR;
+    hf_mmfar = SCB_MMFAR;
+    hf_bfar  = SCB_BFAR;
+    hf_afsr  = SCB_AFSR;
+
+    __asm volatile("bkpt #0");
+
+    while (1) {
+    }
+}
+*/
