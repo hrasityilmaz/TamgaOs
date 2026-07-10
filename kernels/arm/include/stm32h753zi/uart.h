1dcb225c70ba99322c96fd225706302b16267ff4
hrasity <hrasity@gmail.com>
2026-07-10 09:39:51 +0300

    uart updated added functions

--- kernels/arm/include/stm32h753zi/uart.h
+++ kernels/arm/include/stm32h753zi/uart.h
@@ -5,6 +5,9 @@ void uart_init(void);
 void uart_putc(char c);
 void uart_puts(const char *s);
 char uart_getc(void);
-void uart_printf(const char *fmt, ...);
+//void uart_printf(const char *fmt, ...);
+void uart_print_int(const char *prefix, int val);
+void uart_print_2int(const char *prefix, int a, const char *sep, int b);
+void uart_print_imu(int ax, int ay, int az, int gx, int gy, int gz);
 
 #endif
