7d04cec04bd7f810ba42e61466705e358ba9c16a
hrasity <hrasity@gmail.com>
2026-07-16 11:27:36 +0300

    fdcan header file

---
+++ kernels/arm/include/stm32h753zi/fdcan.h
@@ -0,0 +1,18 @@
+#ifndef FDCAN_H
+#define FDCAN_H
+
+#include <stdint.h>
+
+/* FDCAN frame type */
+typedef struct {
+    uint32_t id;        /* 11-bit CAN ID */
+    uint8_t  dlc;       /* data length 0-8 */
+    uint8_t  data[8];
+} fdcan_frame_t;
+
+void    fdcan_init(void);
+int8_t  fdcan_transmit(const fdcan_frame_t *frame);
+int8_t  fdcan_receive(fdcan_frame_t *frame);
+uint8_t fdcan_rx_pending(void);
+
+#endif /* FDCAN_H */
