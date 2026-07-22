2009c27556e47504050fcd0a08d0eb86fe9077a7
hrasity <hrasity@gmail.com>
2026-07-22 10:14:36 +0300

    stm new files added to boardmk

--- kernels/arm/boards/Stm32h753zi/Board.mk
+++ kernels/arm/boards/Stm32h753zi/Board.mk
@@ -16,23 +16,29 @@ AS_SRCS += kernel/arch/cortex_m7/pendsv_handler.s
 C_SRCS  += kernel/core/scheduler.c
 C_SRCS  += kernel/core/mutex.c
 C_SRCS  += kernel/core/semaphore.c
+C_SRCS  += kernel/core/queue.c
+# For tests close main and open only one test all have main() !!
 C_SRCS  += drivers/stm32h753zi/uart.c
-
 C_SRCS  += drivers/stm32h753zi/rcc.c
 C_SRCS += drivers/stm32h753zi/systick.c
 C_SRCS += drivers/stm32h753zi/i2c.c
-C_SRCS += src/stm32h753zi/main.c
+# C_SRCS += src/stm32h753zi/main.c
+# C_SRCS += tests/test_queue_priority_order.c
 # C_SRCS += tests/test_mpu_stack_guard.c
+# C_SRCS += tests/test_timeout.c
+# C_SRCS += tests/test_event_flags.c
+C_SRCS += tests/stm/test_fdcan_real_bus.c
+#C_SRCS += tests/stm/test_fdcan_loopback_final.c
 C_SRCS += sensors/mpu6050.c
 C_SRCS += drivers/stm32h753zi/fdcan.c
 C_SRCS += controls/kalman.c
 C_SRCS += kernel/core/hardfault.c
 C_SRCS += kernel/core/fault_log.c
+C_SRCS += kernel/core/event.c
 C_SRCS += drivers/stm32h753zi/iwdg.c
 
 
 PYOCD_TARGET = stm32h743xx
-# CFLAGS += -Ikernels/arm/drivers/stm32h753zi
 BOARD_INCLUDES += -Iinclude/stm32h753zi
 BOARD_INCLUDES += -Iinclude/sensors
 BOARD_INCLUDES += -Iinclude/controls
