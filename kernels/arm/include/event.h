3ad401fb195c1e1820a8a1eca30082d0a6b19cd0
hrasity <hrasity@gmail.com>
2026-07-26 09:18:31 +0300

    isr functions

--- kernels/arm/include/event.h
+++ kernels/arm/include/event.h
@@ -22,6 +22,18 @@ typedef enum {
   EVENT_WAIT_ALL = 1
 } event_wait_mode_t;
 
+/* ISR SAFE */
+
+static inline void event_set_from_isr(event_group_t *e, uint32_t bits_to_set)
+{
+    event_set(e, bits_to_set);
+}
+
+static inline void event_clear_from_isr(event_group_t *e, uint32_t bits_to_clear)
+{
+    event_clear(e, bits_to_clear);
+}
+
 void event_init(event_group_t *e);
 void event_set(event_group_t *e, uint32_t bits_to_set);
 void event_clear(event_group_t *e, uint32_t bits_to_clear);
