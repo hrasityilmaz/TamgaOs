988e0b78283359ebf98f440a2618a4009690d090
hrasity <hrasity@gmail.com>
2026-07-26 09:20:29 +0300

    tickless functions

--- kernels/arm/include/stm32h753zi/systick.h
+++ kernels/arm/include/stm32h753zi/systick.h
@@ -6,6 +6,7 @@
 void systick_init(uint32_t core_clock_hz);
 void systick_delay_ms(uint32_t ms);
 uint32_t systick_get_ms(void);
-void systick_tick_increment(void); 
+void systick_tick_increment(void);
+void systick_isr_hook(void) __attribute__((weak));
 
 #endif /* SYSTICK_H */
