d3b16e05c66f05b0f33c842edde4e8cc399d234b
hrasity <hrasity@gmail.com>
2026-08-02 16:20:22 +0300

    network stm files header

---
+++ kernels/arm/include/network/stm32h753zi/eth.h
@@ -0,0 +1,12 @@
+#ifndef ETH_H
+#define ETH_H
+
+#include <stdint.h>
+
+void eth_init(uint8_t loopback_mode);
+int8_t eth_transmit(const uint8_t *data, uint16_t len);
+int8_t eth_receive(uint8_t *buf, uint16_t max_len, uint16_t *out_len);
+uint16_t mdio_read(uint8_t phy_addr, uint8_t reg_addr);
+void mdio_write(uint8_t phy_addr, uint8_t reg_addr, uint16_t value);
+
+#endif /* ETH_H */
