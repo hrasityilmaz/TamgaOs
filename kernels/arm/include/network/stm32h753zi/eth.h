143d4e7e66b13124f192d273f8050169c6c9d299
turuk <hrasity@gmail.com>
2026-08-04 13:37:01 +0300

    lin was no err on win error soo fixed

--- kernels/arm/include/network/stm32h753zi/eth.h
+++ kernels/arm/include/network/stm32h753zi/eth.h
@@ -8,5 +8,6 @@ int8_t eth_transmit(const uint8_t *data, uint16_t len);
 int8_t eth_receive(uint8_t *buf, uint16_t max_len, uint16_t *out_len);
 uint16_t mdio_read(uint8_t phy_addr, uint8_t reg_addr);
 void mdio_write(uint8_t phy_addr, uint8_t reg_addr, uint16_t value);
+void eth_init_link_only(void);
 
 #endif /* ETH_H */
