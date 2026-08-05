632822d15d77b98999a9a3af939bdff0be3aad13
turuk <hrasity@gmail.com>
2026-08-05 17:54:31 +0300

    eth functions

--- kernels/arm/include/network/stm32h753zi/eth.h
+++ kernels/arm/include/network/stm32h753zi/eth.h
@@ -9,5 +9,11 @@ int8_t eth_receive(uint8_t *buf, uint16_t max_len, uint16_t *out_len);
 uint16_t mdio_read(uint8_t phy_addr, uint8_t reg_addr);
 void mdio_write(uint8_t phy_addr, uint8_t reg_addr, uint16_t value);
 void eth_init_link_only(void);
+// Just for test
+uint32_t eth_debug_read_dmacsr(void); 
+void eth_debug_clear_dmacsr(uint32_t mask);
+uint32_t eth_debug_read_rx_des3(uint8_t index);
+void eth_debug_dump_registers(void);
+
 
 #endif /* ETH_H */
